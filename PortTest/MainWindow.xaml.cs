f1f6a94c904df4b6e8484af5a8477a4fe0c0ae0d
ives <renhanlinbsl@163.com>
2019-05-16 16:39:47 +0800

    v1.0

---
+++ PortTest/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace PortTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly object lockObj = new object();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Btn_Test(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(startIP.Text) || string.IsNullOrEmpty(endIP.Text) ||
+                string.IsNullOrEmpty(startPort.Text) || string.IsNullOrEmpty(endPort.Text) ||string.IsNullOrEmpty(timeOut.Text))
+            {
+                MessageBox.Show("请输入完整");
+                return;
+            }
+            //check ip 
+            if (!Utility.CheckIP(startIP.Text.Trim()))
+            {
+                MessageBox.Show("开始ip错误");
+                return;
+            }
+            if (!Utility.CheckIP(endIP.Text.Trim()))
+            {
+                MessageBox.Show("结束ip错误");
+                return;
+            }
+
+            //将ip转为ulong
+            //开始ip
+            var startIPValue = Utility.IP2ULong(startIP.Text.Trim());
+            //结束ip
+            var endIPValue = Utility.IP2ULong(endIP.Text.Trim());
+            if (startIPValue > endIPValue)
+            {
+                MessageBox.Show("开始ip大于结束ip");
+                return;
+            }
+
+            var currentIPValue = startIPValue;
+
+            var startPortValue = int.Parse(startPort.Text);
+            var endPortValue = int.Parse(endPort.Text);
+            if (startPortValue > endPortValue)
+            {
+                MessageBox.Show("开始端口大于结束端口");
+                return;
+            }
+            var timeOutValue = int.Parse(timeOut.Text.Trim());
+            while (currentIPValue <= endIPValue)
+            {
+                //loop test port
+                var currentPortValue = int.Parse(startPort.Text);
+                while (currentPortValue <= endPortValue)
+                {
+                    var ip = Utility.ULong2IP(currentIPValue);
+                    var tmpPort = currentPortValue;
+                    Task.Run(() =>
+                    {
+                        Test(ip, tmpPort, timeOutValue);
+                    });
+                    currentPortValue++;
+                }
+                currentIPValue++;
+            }
+        }
+
+        private void Test(string ipStr, int port,int timeoutValue)
+        {
+            IPAddress ip = IPAddress.Parse(ipStr);
+            IPEndPoint point = new IPEndPoint(ip, port);
+
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var result = s.BeginConnect(point, null, null);
+            result.AsyncWaitHandle.WaitOne(timeoutValue, true);
+            if (!result.IsCompleted)
+            {
+                AddLog($"【{ipStr}:{port}】:timeout");
+                s.Close();
+            }
+            else
+            {
+                AddLog($"【{ipStr}:{port}】:success");
+                s.Close();
+            }
+            //2
+            //try
+            //{
+            //    TcpClient tcp = new TcpClient();
+            //    tcp.Connect(point);
+            //    MessageBox.Show("端口打开");
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+        }
+
+        private void AddLog(string value)
+        {
+            Dispatcher.Invoke(new Action(() =>
+            {
+                resultListView.Items.Add(value);
+                resultListView.SelectedIndex = resultListView.Items.Count - 1;
+                resultListView.ScrollIntoView(resultListView.SelectedItem);
+            }));
+        }
+        private void Number_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Regex.IsMatch(e.Text, @"^\d*$"))
+            {
+                MessageBox.Show("输入有误");
+                e.Handled = true;
+            }
+        }
+        #region 焦点复制
+        private void endIP_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(startIP.Text))
+            {
+                if (string.IsNullOrEmpty(endIP.Text))
+                {
+                    endIP.Text = startIP.Text;
+                }
+            }
+        }
+
+        private void endPort_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(startPort.Text))
+            {
+                if (string.IsNullOrEmpty(endPort.Text))
+                {
+                    endPort.Text = startPort.Text;
+                }
+            }
+        }
+        #endregion
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            new PortTest.About().ShowDialog();
+        }
+    }
+}
---
+++ PortTest/Utility.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace PortTest
+{
+    public class Utility
+    {
+        public static bool CheckIP(string text)
+        {
+            //匹配ip
+            if (!Regex.IsMatch(text, @"(^([1-9]|1[0-9]|1[1-9]{2}|2[0-4][0-9]|25[0-5])\.)(([0-9]{1,2}|1[1-9]{2}|2[0-4][0-9]|25[0-5])\.){2}([0-9]{1,2}|1[1-9]{2}|2[0-5][0-9]|25[0-4])$"))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static ulong IP2ULong(string ip)
+        {
+            if (!CheckIP(ip))
+            {
+                //ip error
+                MessageBox.Show("ip错误");
+            }
+            List<ulong> data = new List<ulong>();
+            var ips = ip.Split('.');
+            foreach (var item in ips)
+            {
+                data.Add(ulong.Parse(item));
+            }
+            ulong result = 0;
+            ulong first = data[0] * 256 * 256 * 256;
+            ulong second = data[1] * 256 * 256;
+            ulong third = data[2] * 256;
+            result = first + second + third + data[3];
+            return result;
+        }
+
+        public static string ULong2IP(ulong ip)
+        {
+            ulong tmp = ip;
+            ulong last = tmp % 256;
+            tmp = tmp - last;
+            ulong third = tmp % (256 * 256)/256;
+            tmp = tmp - third;
+            ulong second = tmp % (256 * 256 * 256)/256/256;
+            tmp = tmp - third;
+            ulong first = tmp % ((ulong)256 * 256 * 256 * 256)/256/256/256;
+
+            return $"{first}.{second}.{third}.{last}";
+        }
+    }
+}
